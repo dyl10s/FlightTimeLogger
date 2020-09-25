2b2a34e41aaf9381ce64c4e2af8214a2389e4c7a
Dylan Strohschein <dylanstrohschein@gmail.com>
2020-09-25 15:56:53 -0400

    Add project files.

---
+++ FlightTimeLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightTimeLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
